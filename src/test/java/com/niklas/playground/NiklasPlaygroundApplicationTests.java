78c35c0021d9b32e5853add6e0e0d38c71c3f6d3
Niklas <niklas.kufs@otto.de>
2025-08-18 14:13:20 +0200

    Initial commit

---
+++ src/test/java/com/niklas/playground/NiklasPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niklas.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiklasPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
